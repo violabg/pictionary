942d18bf5b08dfe4f663ee8c872317878b213ebf
Paolo Bernardini <p.bernardini@mediacross.it>
2024-11-12 09:04:30 +0100

    Refactor: separate Pictionary layout into PictionaryContent component for improved readability

--- src/components/Pictionary/index.tsx
+++ src/components/Pictionary/index.tsx
@@ -1,19 +1,34 @@
 "use client";
 
+import { isDrawerAtom } from "@/atoms";
 import { jotaiStore } from "@/atoms/store";
 import { GameController } from "@/components/GameController";
-import { Provider } from "jotai";
+import { Provider, useAtomValue } from "jotai";
 import Whiteboard from "../Whiteboard";
+
+function PictionaryContent() {
+  const isDrawer = useAtomValue(isDrawerAtom);
+
+  return (
+    <div
+      className={`gap-2 grid grid-cols-[300px_1fr] ${
+        isDrawer
+          ? "grid-rows-[auto_1fr] [grid-template-areas:'sidebar_header'_'sidebar_content']"
+          : "grid-rows-[1fr] [grid-template-areas:'sidebar_content']"
+      } p-2 h-[100vh]`}
+    >
+      <aside className="[grid-area:sidebar]">
+        <GameController />
+      </aside>
+      <Whiteboard />
+    </div>
+  );
+}
 
 export default function Pictionary() {
   return (
     <Provider store={jotaiStore}>
-      <div className="gap-2 grid grid-cols-[300px_1fr] grid-rows-[auto_1fr] [grid-template-areas:'sidebar_header'_'sidebar_content'] p-2 h-[100vh]">
-        <aside className="[grid-area:sidebar]">
-          <GameController />
-        </aside>
-        <Whiteboard />
-      </div>
+      <PictionaryContent />
     </Provider>
   );
 }
