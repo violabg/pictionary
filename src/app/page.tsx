787a01c4ed24387da6ee00dc2b9a9e5b38762a9c
Paolo Bernardini <p.bernardini@mediacross.it>
2025-04-14 17:39:48 +0200

    upgraded to tailwind v4

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Pictionary from "@/components/game/Pictionary";
 
 export default function Home() {
   return (
-    <div className="min-h-screen font-[family-name:var(--font-geist-sans)]">
+    <div className="min-h-screen ">
       <Pictionary />
     </div>
   );
